a9698c236fb5657ef382da8e1bb46157abace217
Nicole Maguire <maguirne@dukes.jmu.edu>
2018-04-15 18:45:58 -0400

    Made connection with Cantus2017 database

--- src/SQLHandler.java
+++ src/SQLHandler.java
@@ -1,11 +1,25 @@
 import java.sql.*;
 
 public class SQLHandler {
+	private Connection connection;
+	private Statement statement;
+	
 	public SQLHandler() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			//Debug print, remove for final submission
+			System.out.println("Driver loaded");
 			
-			System.out.println("loaded the driver");
+			try {
+				connection = DriverManager.getConnection("jdbc:mysql://mysql.cs.jmu.edu/Cantus2017", "maguirne", "Madison609!");
+				System.out.println("Connection Made");
+				
+				statement = connection.createStatement();
+			} catch (SQLException error) {
+				System.out.println("Could not make the connection");
+			}
+			
+			
 		} catch (Exception error) {
 			System.out.println("Could not load the driver : " + error.getMessage());
 		}
